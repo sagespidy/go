ced7a64e14112dffa7d1535973bcbe6e040ca994
shailender choudhary <shailender.choudhary@rivigo.com>
2019-12-22 13:12:29 +0530

    day1! Hello world

--- hw.go
+++
@@ -1,7 +0,0 @@
-package main  // 1. Must declare package
-import "fmt"  //2. import package, also  called  preprocessor command which tell go to compile files lying in package fmt
-
-func main () /*3.main function where execution starts */{
-//4. This my friend is a comment !!
-fmt.Println("Hello, World!")  // another function in go where fmt package call Println method
-}
